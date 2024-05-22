c42c04fad87cd91bdab3fe92848bd96ad6a167af
Gent <82123790+TheGent@users.noreply.github.com>
2024-05-22 22:29:17 +0200

    Application Name changed
    Changed Officially to Project64 Version 1.6 Plus as has been verified as fixing the Vulnerability.

--- Main.h
+++ Main.h
@@ -46,11 +46,11 @@ extern "C" {
 #define AppName  "Project64 (Build 57)"
 #else
 //#define BETA_VERSION
-#define AppVer   "Project64 - Version 1.6 Plus (Vunerbility Fix RC1)"
+#define AppVer   "Project64 - Version 1.6 Plus"
 #ifdef BETA_VERSION
 #define AppName  "Project64 Version 1.6 Plus (Vunerbility Fix RC1)"
 #else
-#define AppName  "Project64 Version 1.6 Plus (Vunerbility Fix RC1)"
+#define AppName  "Project64 Version 1.6 Plus"
 #endif
 #endif
 
